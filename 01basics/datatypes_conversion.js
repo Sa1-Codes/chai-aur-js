3ced1ebf60790642522ca1a521f903fec97847b4
Sa1-Codes <149556870+Sa1-Codes@users.noreply.github.com>
2024-05-07 05:16:36 +0000

    datatypeConversion

---
+++ 01basics/datatypes_conversion.js
@@ -0,0 +1,31 @@
+let num  = 33
+let numtostr = String(num);
+
+console.log(typeof numtostr);
+
+let str = "73bskd"
+
+let strToNum = Number (str)
+
+console.log(typeof strToNum)
+console.log(strToNum)
+
+// "33" => 33
+// 33=>"33"
+// "73bskd" => NaN
+// true => 1;
+// null => 0
+
+let loggedIn = 1;
+let boolLoggedIn = Boolean (loggedIn)
+console.log(boolLoggedIn)
+
+//1 => true
+// 0 => false
+
+let custname = "sawan"
+let boolname = Boolean(custname)
+console.log(boolname)
+
+//"sawan" = true
+// ""=> false 
