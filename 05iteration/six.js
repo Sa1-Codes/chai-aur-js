2b5223b1c10b8f164facf748a6510c0bf09e0475
Sa1-Codes <149556870+Sa1-Codes@users.noreply.github.com>
2024-05-11 18:23:51 +0000

    filter map reduce

---
+++ 05iteration/six.js
@@ -0,0 +1,18 @@
+const lang = ["js","ruby","java","python","cpp"]
+
+// const values = lang.forEach((item)=>{
+//     console.log(item)    // forEach does not return value
+//     return item
+// })
+
+// console.log(values)
+
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const satisfiedNum = myNums.filter((num)=>{
+//     return num>4
+// })
+
+// console.log(satisfiedNum)
+
