9895b1f3c60db615df5c2db418b6f5ad21964926
Sa1-Codes <149556870+Sa1-Codes@users.noreply.github.com>
2024-05-11 15:56:30 +0000

    while loop

---
+++ 05iteration/two.js
@@ -0,0 +1,21 @@
+
+let index = 0;
+// while (index<=10) {
+//    console.log(index)
+//    index+=2
+// }
+
+let myArray = ["flash","batman","superman"]
+let arr = 0;
+while (arr<myArray.length) {
+    console.log(`${myArray[arr]}`)
+    arr++;
+}
+
+let score  =1;
+
+
+do {
+    console.log(`score id ${score}`)
+    score++
+} while (score<5);
